3bc49d6107cae312bd2ae7e8389ec55e1c642c05
haseebasim <haseebzaidi123@gmail.com>
2020-07-25 00:26:47 +0500

    Auth check

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:3001",
+  baseURL: "https://szfood-back-end.herokuapp.com",
   responseType: "json",
 });
