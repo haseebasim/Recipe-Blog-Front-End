5a15b70512fe184c9596e8a0914146b6226bbdab
Alam Khalid <alam.khalid@devsinc.com>
2022-05-28 16:52:15 +0500

    Home page loader spinner fix

--- src/Redux/actions/homeAction.js
+++ src/Redux/actions/homeAction.js
@@ -1,14 +1,21 @@
 import { FETCH_HOME_POSTS } from "./types";
 import axios from '../../utils/Axios'
 export const fetchHomePosts = () => (dispatch) => {
-  axios
-    .get(`/posts/${3}/${0}`)
-    .then((res) =>
-      dispatch({
-        type: FETCH_HOME_POSTS,
-        payload: res.data,
-        loading: false
-      })
-    )
-    .catch((error) => console.log(error));
+    axios
+        .get(`/posts/${3}/${0}`)
+        .then((res) =>
+            dispatch({
+                type: FETCH_HOME_POSTS,
+                payload: res.data,
+                loading: false
+            })
+        )
+        .catch((error) => {
+            dispatch({
+                type: FETCH_HOME_POSTS,
+                payload: [],
+                loading: false
+            })
+            console.log(error);
+        });
 };
