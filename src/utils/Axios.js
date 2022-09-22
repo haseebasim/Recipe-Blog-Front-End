dcfc636fb79085f7eeb0edaf924c3017755fc31c
Syed Haseeb Asim Zaidi <45163316+haseebasim@users.noreply.github.com>
2022-09-22 17:38:06 +0500

    Update Axios.js

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: "https://szfood-back-end.herokuapp.com",
+  baseURL: "https://sz-food-backend.herokuapp.com",
   responseType: "json",
 });
